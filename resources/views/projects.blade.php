35ba48ed46accd24f044373dc8ce683f1afda8bd
Alain <alain.gutjahr@students.ffhs.ch>
2020-08-20 13:58:31 +0200

    Added Sites

---
+++ resources/views/projects.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Projects</div>
+
+                    <div class="card-body">
+                        Here comes a projects list
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('dashboard');
+});
+Route::get('/projects', function () {
+    return view('projects');
+});
+Route::get('/users', function () {
+    return view('users');
 });
 
 Auth::routes();
