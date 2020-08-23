a54cafe8c671ee43f0b3597c55b7a4867a35da3d
Alain <alain.gutjahr@students.ffhs.ch>
2020-08-23 17:03:30 +0200

    Added delete()

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -133,8 +133,12 @@ class ProjectsController extends Controller
      * @param  \App\projects  $projects
      * @return \Illuminate\Http\Response
      */
-    public function destroy(projects $projects)
+    public function destroy(projects $project)
     {
-        //
+        $deleted_project = $project->projectname;
+        $project->delete();
+        return redirect('/projects')->with(
+            'msg_success' , '<i class="fa fa-check-circle"></i><span class="pl-2" style="font-weight: bold; text-transform: capitalize;">' . $deleted_project . '</span> gelöscht'
+        );
     }
 }
--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -9,14 +9,23 @@
 
                     <div class="card-body">
                         <ul class="list-group mb-3">
-                            @foreach($projects as $project)
-                                <li class="list-group-item">{{ $project->projectname }}
-                                    <a href="projects/{{ $project->id }}"><i class="fa fa-eye pl-2"></i></a>
-                                    <a href="projects/{{ $project->id }}/edit"><i class="fa fa-edit pl-2"></i></a>
-                                </li>
-                            @endforeach
+                            @if(count($projects) > 0)
+                                @foreach($projects as $project)
+                                    <li class="list-group-item">{{ $project->projectname }}
+                                        <a href="projects/{{ $project->id }}"><i class="fa fa-eye pl-2"></i></a>
+                                        <a href="projects/{{ $project->id }}/edit"><i class="fa fa-edit pl-2"></i></a>
+                                        <form id="delete-form" style="display: inline;" action="/projects/{{ $project->id }}" method="post">
+                                            @csrf
+                                            @method('DELETE')
+                                            <button class="btn btn-link text-danger" type="submit"><i class="fa fa-trash text-danger" ></i></button>
+                                        </form>
+                                    </li>
+                                @endforeach
+                            @else
+                                <p>Keine Projekte vorhanden</p>
+                            @endif
                         </ul>
-                        <a href="projects/create" class="btn btn-primary"><i class="fas fa-plus-circle pr-3"></i>Add Porject</a>
+                        <a href="projects/create" class="btn btn-primary"><i class="fas fa-plus-circle pr-3"></i>Add Project</a>
                     </div>
                 </div>
             </div>
